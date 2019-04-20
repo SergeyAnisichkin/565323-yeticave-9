d2da111141876eff2fcc3eac8c122d02ffcbe325
Sergey Anisichkin <s.anisichkin@gmail.com>
2019-04-20 13:04:03 +0300

    Добавил функцию таймера

--- templates/index.php
+++ templates/index.php
@@ -32,8 +32,8 @@
                             <span class="lot__amount">Стартовая цена</span>
                             <span class="lot__cost"><?=format_cost($lot['cost'])?></span>
                         </div>
-                        <div class="lot__timer timer ">
-                            12:33
+                        <div class="lot__timer timer <?= (calc_timer()->h < 1) ? "timer--finishing" : "" ?>">
+                            <?= date_interval_format(calc_timer(), "%h:%i") ?>
                         </div>
                     </div>
                 </div>
