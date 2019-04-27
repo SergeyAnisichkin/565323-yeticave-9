2c63754f283036427596a7297b5857c2680cede7
Sergey Anisichkin <s.anisichkin@gmail.com>
2019-04-27 13:14:41 +0300

    Добавил запросы массивов категорий и лотов из БД

--- index.php
+++ index.php
@@ -3,45 +3,6 @@ require_once "helpers.php";
 
 $is_auth = rand(0, 1);
 $user_name = 'Сергей';
-$categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
-$lots = [
-    [
-        'title' => "2014 Rossignol District Snowboard",
-        'category' => "Доски и лыжи",
-        'cost' => 10999,
-        'img' => "img/lot-1.jpg",
-    ],
-    [
-        'title' => "DC Ply Mens 2016/2017 Snowboard",
-        'category' => "Доски и лыжи",
-        'cost' => 159999,
-        'img' => "img/lot-2.jpg",
-    ],
-    [
-        'title' => "Крепления Union Contact Pro 2015 года размер L/XL",
-        'category' => "Крепления",
-        'cost' => 8000,
-        'img' => "img/lot-3.jpg",
-    ],
-    [
-        'title' => "Ботинки для сноуборда DC Mutiny Charocal",
-        'category' => "Ботинки",
-        'cost' => 10999,
-        'img' => "img/lot-4.jpg",
-    ],
-    [
-        'title' => "Куртка для сноуборда DC Mutiny Charocal",
-        'category' => "Одежда",
-        'cost' => 7500,
-        'img' => "img/lot-5.jpg",
-    ],
-    [
-        'title' => "Маска Oakley Canopy",
-        'category' => "Разное",
-        'cost' => 10999,
-        'img' => "img/lot-6.jpg",
-    ],
-];
 
 function format_cost($cost) {
     $cost = ceil($cost);
@@ -57,17 +18,33 @@ function calc_timer() {
     return $diff;
 }
 
-$page_content = include_template('index.php', [
-    'lots' => $lots,
-    'categories' => $categories
-]);
+$link = mysqli_connect("localhost", "root", "", "yeticave");
+if (!$link) {
+    $layout_content = "Ошибка подключения. " . mysqli_connect_error();
+}
+else {
+    $sql = "SELECT name, code FROM categories";
+    $result = mysqli_query($link, $sql);
+    $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-$layout_content = include_template('layout.php', [
-    'content' => $page_content,
-    'title' => "YetiCave - Главная",
-    'categories' => $categories,
-    'is_auth' => $is_auth,
-    'user_name' => $user_name,
-]);
+    $sql = "SELECT l.title, l.img_url, l.start_cost, c.name AS category " .
+    "FROM lots AS l LEFT JOIN categories AS c " . 
+    "ON l.category_id = c.id WHERE l.winner_user_id IS NULL;";
+    $result = mysqli_query($link, $sql);
+    $lots = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    $page_content = include_template('index.php', [
+        'lots' => $lots,
+        'categories' => $categories,
+    ]);
+    
+    $layout_content = include_template('layout.php', [
+        'content' => $page_content,
+        'title' => "YetiCave - Главная",
+        'categories' => $categories,
+        'is_auth' => $is_auth,
+        'user_name' => $user_name,
+    ]);
+}
 
 print($layout_content);
--- templates/index.php
+++ templates/index.php
@@ -5,8 +5,8 @@
     <ul class="promo__list">
         <!--заполните этот список из массива категорий-->
         <?php foreach ($categories as $category): ?>
-            <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?=htmlspecialchars($category)?></a>
+            <li class="promo__item promo__item--<?=$category['code']?>">
+                <a class="promo__link" href="pages/all-lots.html"><?=$category['name']?></a>
             </li>
         <?php endforeach; ?>
     </ul>
@@ -20,17 +20,17 @@
         <?php foreach ($lots as $lot): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$lot['img']?>" width="350" height="260" alt="">
+                    <img src="<?=$lot['img_url']?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=htmlspecialchars($lot['category'])?></span>
+                    <span class="lot__category"><?=$lot['category']?></span>
                     <h3 class="lot__title">
                         <a class="text-link" href="pages/lot.html"><?=htmlspecialchars($lot['title'])?></a>
                     </h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=format_cost($lot['cost'])?></span>
+                            <span class="lot__cost"><?=format_cost($lot['start_cost'])?></span>
                         </div>
                         <div class="lot__timer timer <?= (calc_timer()->h < 1) ? "timer--finishing" : "" ?>">
                             <?= date_interval_format(calc_timer(), "%H:%I") ?>
